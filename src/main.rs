a9d991de2716146de643c399fe5b794ae087f450
Alistair A. Israel <aisrael@gmail.com>
2020-04-22 15:23:45 +0800

    Use the new async model

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use futures::future;
-use futures::future::IntoFuture;
-use hyper::rt::Future;
-use hyper::service::{NewService, Service};
-use hyper::{Body, Request, Response, Server, StatusCode};
+use hyper::service::{make_service_fn, Service};
+use hyper::{Body, Request, Response, Server};
+use std::convert::Infallible;
+use std::future::Future;
+use std::pin::Pin;
 use std::sync::Arc;
+use std::task::Context;
+use std::task::Poll;
 
 struct Config {
     message: String,
@@ -14,66 +16,49 @@ struct TestService {
     config: Arc<Config>,
 }
 
-impl Service for TestService {
-    type ReqBody = Body;
-    type ResBody = Body;
+impl Service<Request<Body>> for TestService {
+    type Response = Response<Body>;
     type Error = http::Error;
-    type Future = Box<dyn Future<Item = Response<Body>, Error = Self::Error> + Send>;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
 
-    fn call(&mut self, _req: Request<Self::ReqBody>) -> Self::Future {
-        Box::new(
-            Response::builder()
-                .status(StatusCode::OK)
-                .body(format!("Hello, {}!", &self.config.message).into())
-                .into_future(),
-        )
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, _req: Request<Body>) -> Self::Future {
+        let s = format!("Hello, {}!", self.config.message);
+        let response = Response::new(Body::from(s));
+
+        // create a response in a future.
+        let fut = async { Ok(response) };
+
+        // Return the response as an immediate future
+        Box::pin(fut)
     }
 }
 
-/// The server
-struct TestServer {
-    config: Arc<Config>,
-}
-
-impl NewService for TestServer {
-    type ReqBody = Body;
-    type ResBody = Body;
-    type Error = http::Error;
-    type InitError = http::Error;
-    type Future = Box<dyn Future<Item = Self::Service, Error = Self::Error> + Send>;
-    type Service = TestService;
-    fn new_service(&self) -> Self::Future {
-        Box::new(future::ok(TestService {
-            config: self.config.clone(),
-        }))
-    }
-}
-
-impl TestServer {
-    fn new(config: Config) -> Self {
-        TestServer {
-            config: Arc::new(config),
-        }
-    }
-
-    fn start(self) -> ! {
-        // This is our socket address...
-        let addr = ([127, 0, 0, 1], 4000).into();
-
-        let server = Server::bind(&addr)
-            .serve(self)
-            .map_err(|e| eprintln!("server error: {}", e));
-
-        // Run this server for... forever!
-        hyper::rt::run(server);
-        std::process::exit(0)
-    }
-}
-
-fn main() {
-    let config = Config {
+#[tokio::main]
+async fn main() {
+    let config = Arc::new(Config {
         message: "world".to_owned(),
-    };
-    let server: TestServer = TestServer::new(config);
-    server.start();
+    });
+
+    // Spawn the root task
+    let make_svc = make_service_fn(move |_conn| {
+        let clone = Arc::clone(&config);
+        async {
+            let service = TestService { config: clone };
+            Ok::<_, Infallible>(service)
+        }
+    });
+
+    // This is our socket address...
+    let addr = ([127, 0, 0, 1], 4000).into();
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    // Run this server for... forever!
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
 }
